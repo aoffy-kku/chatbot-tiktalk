081877b283469c0263615eadedbbff540eb8369e
aoffy-kku <tanapat_pt@kkumail.com>
2018-06-05 17:14:03 +0700

    Create Messenger Webhook

---
+++ index.js
@@ -0,0 +1,44 @@
+'use-strict';
+
+const
+  express = require('express');
+bodyParser = require('body-parser');
+app = express().use(bodyParser.json());
+const PORT = process.env.PORT || 1337;
+
+app.post('/webhook', (req, res) => {
+  let body = req.body;
+
+  if (body.object === 'page') {
+    body.entry.forEach(function (entry) {
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.get('/webhook', (req, res) => {
+
+  let VERIFY_TOKEN = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  if (mode && token) {
+
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+
+    } else {
+      res.sendStatus(403);
+    }
+  }
+});
+
+app.listen(PORT, () => console.log(`Messenger Webhook is listening on PORT ${PORT}`));
