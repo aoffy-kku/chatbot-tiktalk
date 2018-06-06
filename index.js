78282d064453f1d3252f51354c6151d5f935c9fb
aoffy-kku <tanapat_pt@kkumail.com>
2018-06-06 11:54:17 +0700

    Update nlp

--- index.js
+++ index.js
@@ -33,53 +33,54 @@ app.post('/webhook', (req, res) => {
 
       let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid);
+      const message = webhook_event.message;
+      if (message) {
+        if (message.text) {
+          // Facebook NLP
+          const msg = "";
+          const greeting = firstEntities(message.nlp, 'greetings');
+          const thank = firstEntities(message.nlp, 'thanks');
+          const bye = firstEntities(message.nlp, 'bye');
+          const datetime = firstEntities(message.nlp, 'datetime');
+          const amount_of_money = firstEntities(message.nlp, 'amount_of_money');
+          const phone_number = firstEntities(message.nlp, 'phone_number');
+          const email = firstEntities(message.nlp, 'email');
+          const quantity = firstEntities(message.nlp, 'quantity');
+          const distance = firstEntities(message.nlp, 'distance');
+          const temperature = firstEntities(message.nlp, 'temperature');
+          const volume = firstEntities(message.nlp, 'volume');
+          const location = firstEntities(message.nlp, 'location');
+          const url = firstEntities(message.nlp, 'url');
+          const sentiment = firstEntities(message.nlp, 'sentiment');
+          const duration = firstEntities(message.nlp, 'duration');
 
-      if (webhook_event.message) {
-        if (webhook_event.message.text) {
+          if (greeting && greeting.confidence > 0.8) {
+            msg = "Hi, what can i help you?";
+          } else if (thank && thank.confidence > 0.8) {
+            msg = "You’re Welcome. :)";
+          } else if (bye && bye.confidence > 0.8) {
+            msg = "Good bye.";
+          } else if (datetime && datetime.confidence > 0.8) {
+            msg = datetime.value;
+          } else if (localtion && localtion.confidence > 0.8) {
+            msg = location.value;
+          } else if (email && email.confidence > 0.8) {
+            msg = email.value;
+          } else if (temperature && temperature.confidence > 0.8) {
+            msg = temperature.value;
+          } else if (url && url.confidence > 0.8) {
+            msg = url.value;
+          } else {
+            msg = "Please contact us at contact@takemetour.com"
+          }
+          //handleMessage(sender_psid, webhook_event.message);
+          handleMessage(sender_psid, msg);
           // Send message to Wit.ai
-          client.message(webhook_event.message.text, {})
-          .then((data) => {
-            console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
-            const msg = "";
-            const greeting = firstEntities(message.nlp, 'greetings');
-            const thank = firstEntities(message.nlp, 'thanks');
-            const bye = firstEntities(message.nlp, 'bye');
-            const datetime = firstEntities(message.nlp, 'datetime');
-            const amount_of_money = firstEntities(message.nlp, 'amount_of_money');
-            const phone_number = firstEntities(message.nlp, 'phone_number');
-            const email = firstEntities(message.nlp, 'email');
-            const quantity = firstEntities(message.nlp, 'quantity');
-            const distance = firstEntities(message.nlp, 'distance');
-            const temperature = firstEntities(message.nlp, 'temperature');
-            const volume = firstEntities(message.nlp, 'volume');
-            const location = firstEntities(message.nlp, 'location');
-            const url = firstEntities(message.nlp, 'url');
-            const sentiment = firstEntities(message.nlp, 'sentiment');
-            const duration = firstEntities(message.nlp, 'duration');
-
-            if(greeting && greeting.confidence > 0.8) {
-              msg = "Hi, what can i help you?";
-            } else if(thank && thank.confidence > 0.8) {
-              msg = "You’re Welcome. :)";
-            } else if(bye && bye.confidence > 0.8) {
-              msg = "Good bye.";
-            } else if(datetime && datetime.confidence > 0.8) { 
-              msg = datetime.value;
-            } else if(localtion && localtion.confidence > 0.8) {
-              msg = location.value;
-            } else if(email && email.confidence > 0.8) {
-              msg = email.value;
-            } else if(temperature && temperature.confidence > 0.8) {
-              msg = temperature.value;
-            } else if(url && url.confidence > 0.8) { 
-              msg = url.value;
-            } else {
-              msg = "Please contact us at contact@takemetour.com"
-            }
-            //handleMessage(sender_psid, webhook_event.message);
-            handleMessage(sender_psid, msg);
-          })
-          .catch(console.error);
+          client.message(message.text, {})
+            .then((data) => {
+              console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
+            })
+            .catch(console.error);
         }
       } else if (webhook_event.postback) {
         handlePostback(sender_psid, webhook_event.postback);
